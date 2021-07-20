3e7c66d35f9d2b71b434a0304686089908ba21cc
lindseywalker <elliewalkersm@gmail.com>
2021-07-20 18:11:31 -0500

    Add project files.

---
+++ PetStore/Pets/Dog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore.Pets
+{
+    class Dog
+    {
+        //Properties
+        public string Size { get; set; } //auto-property
+        public short Weight { get; set; }
+        public string Name { get; set; }
+
+        //Method Signature
+        public void Bark()
+        {
+            Console.WriteLine($"{Name} barks noisily.");
+        }
+
+        public void Eat(string typeOfFood)
+        {
+            if (typeOfFood.ToLower() == "healthy food")
+            {
+                Weight -= 1;
+            }
+
+            Console.WriteLine($"{Name} just ate {typeOfFood}. {Name} weighs {Weight}.");
+        }
+    }
+}
---
+++ PetStore/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using PetStore.Pets;
+
+namespace PetStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var benny = new Dog();
+            benny.Name = "benny";
+            benny.Size = "Large";
+            benny.Weight = 110;
+            benny.Eat("healthy food");
+
+            var gatsby = new Dog();
+            gatsby.Name = "gatsby";
+            gatsby.Size = "large";
+            gatsby.Weight = 90;
+            gatsby.Bark();
+
+        }
+    }
+}
